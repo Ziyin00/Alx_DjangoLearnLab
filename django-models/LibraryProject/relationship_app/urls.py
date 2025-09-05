23ae4f949829066e19794ce83904bee583e2a071
Ziyin Ab <ziyinab00@gmail.com>
2025-09-05 23:30:08 +0300

    done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,19 +1,29 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+
+from django.contrib import admin
+from django.urls import path, include
+
 from . import views
 from .views import list_books, LibraryDetailView
+from .admin_view import admin_dashboard
+from .librarian_view import librarian_dashboard
+from .member_view import member_dashboard
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
 
-    # user authentication
+    # authentication
     path("register/", views.register, name="register"),
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+
+    # role-based access
+    path("admin-dashboard/", admin_dashboard, name="admin_dashboard"),
+    path("librarian-dashboard/", librarian_dashboard, name="librarian_dashboard"),
+    path("member-dashboard/", member_dashboard, name="member_dashboard"),
 ]
-
-
 
 # LibraryProject/urls.py
 
