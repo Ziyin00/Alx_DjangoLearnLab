0ae5b7fd40174cf213c12f1c958164b18731440c
Ziyin Ab <ziyinab00@gmail.com>
2025-09-28 10:52:54 +0300

    adjustment

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework import filters
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
